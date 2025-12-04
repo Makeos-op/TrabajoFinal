78e3f91913089000291a26f3cd992d4b8a39d6b3
Sebastian Medina <smedinamayo@gmail.com>
2025-12-04 17:37:05 -0500

    Borrado base de datos

--- Dato/Dconductor.cs
+++ Dato/Dconductor.cs
@@ -23,11 +23,11 @@ namespace Dato
             }
         }
 
-        public string InsertarConductor(conductor Conductor)
+        public string InsertarConductor(Conductor conductor)
         {
             return EjecutarFuncion(db =>
             {
-                db.conductor.Add(Conductor);
+                db.conductor.Add(conductor);
                 db.SaveChanges();
                 return "Conductor registrado correctamente.";
             });
@@ -44,32 +44,32 @@ namespace Dato
             });
         }
 
-        public string ModificarConductor(conductor Conductor)
+        public string ModificarConductor(Conductor conductor)
         {
             return EjecutarFuncion(db =>
             {
-                var c = db.conductor.Find(Conductor.idConductor);
+                var c = db.conductor.Find(conductor.idConductor);
                 if (c == null) return "Conductor no encontrado.";
 
-                c.nombre = Conductor.nombre;
-                c.apellido = Conductor.apellido;
-                c.dni = Conductor.dni;
-                c.edad = Conductor.edad;
-                c.nacionalidad = Conductor.nacionalidad;
+                c.nombre = conductor.nombre;
+                c.apellido = conductor.apellido;
+                c.dni = conductor.dni;
+                c.edad = conductor.edad;
+                c.nacionalidad = conductor.nacionalidad;
 
                 db.SaveChanges();
                 return "Conductor modificado correctamente.";
             });
         }
-        public List<conductor> MostrarConductores()
+        public List<Conductor> MostrarConductores()
         {
-            List<conductor> listaconductor = new List<conductor>();
+            List<Conductor> listaconductor = new List<Conductor>();
             try
             {
                 using (var context = new BDFEEntities()) // Crea una instancia del contexto de la base de datos
                 {
                     context.Configuration.LazyLoadingEnabled = false;
-                    listaconductor = context.conductor.ToList(); // Ejecuta la funcion pasada como parametro con el contexto y retorna su resultado
+                    listaconductor = context.Conductor.ToList(); // Ejecuta la funcion pasada como parametro con el contexto y retorna su resultado
                 }
                 return listaconductor;
             }
@@ -78,13 +78,17 @@ namespace Dato
                 return listaconductor;
             }
         }
-        public conductor ObtenerConductorPorDNI(string dni)
+        public Conductor ObtenerConductorPorDNI(string dni)
         {
             return MostrarConductores().FirstOrDefault(c => c.dni == dni);
         }
-        public List<vehiculo> MostrarVehiculos(string dni)
+        public List<Vehiculo> MostrarVehiculos(string dni)
         {
             return ObtenerConductorPorDNI(dni).vehiculo.ToList();
+        }
+        public List<Brevete> MostrarBrevetes(string dni)
+        {
+            return ObtenerConductorPorDNI(dni).Brevete.ToList();
         }
     }
 
